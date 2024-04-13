f57cf56568dab42f8b172d8d23af3e88ade8669c
Cristian Julio <cristian.juliom@udea.edu.co>
2024-04-13 14:43:55 -0500

    Creando entidad Barbería

---
+++ BarberiAppNegocio/Models/Barberia.cs
@@ -0,0 +1,16 @@
+﻿namespace BarberiAppNegocio.Models
+{
+    public class Barberia
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public int StockProducto { get; set; }
+        public int CitaId { get; set; }
+        public int ServicioId { get; set; }
+        public int ProductoId { get; set; }
+        public int AdministradorNegocioId { get; set; }
+        public int EmpleadoBarberiaId { get; set; }
+    }
+}
